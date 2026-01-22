f14dc5e0cb78c399aa41d87cc9940b706d462d9b
Perry Angelora <pea.mail.list@gmail.com>
2026-01-21 22:57:33 -0500

    Separated bluesky and twitter image creations in the event one fails - also bluesky have a 1MB limit.

--- src/imageHandling/createImage.ts
+++ src/imageHandling/createImage.ts
@@ -3,6 +3,11 @@ import { ClusterRecord, Database } from '../types.js';
 import fs from 'fs';
 import path from 'path';
 
+/**
+ * Returns the first row that has pending was_posted_twitter OR was_posted_bluesky
+ * @param db Database
+ * @returns DB row
+ */
 async function _claimNextClusterPost(db: Database): Promise<ClusterRecord | undefined> {
   const row = await db.getData(`
     UPDATE cluster_post
@@ -13,7 +18,7 @@ async function _claimNextClusterPost(db: Database): Promise<ClusterRecord | unde
       SELECT rowid
       FROM cluster_post
       WHERE was_posted_twitter = 'pending'
-        AND was_posted_bluesky = 'pending'
+        OR was_posted_bluesky = 'pending'
       LIMIT 1
     )
     RETURNING *
@@ -21,16 +26,18 @@ async function _claimNextClusterPost(db: Database): Promise<ClusterRecord | unde
   return row;
 }
 
+
 async function _screenshotHtmlToFile(page: puppeteer.Page, htmlString: string, outputPath: string, aspectRatio: string = 'twitter') {
   const width = aspectRatio === 'twitter' ? 1500 : 1500; // twitter: 3/2 - bluesky: 4/3
   const height = aspectRatio === 'twitter' ? 1000 : 1125;
+  const deviceScale = aspectRatio === 'twitter' ? 4 : 3;
 
   await page.goto('about:blank', { waitUntil: 'load' });
 
   await page.setViewport({
     width,
     height,
-    deviceScaleFactor: 4
+    deviceScaleFactor: deviceScale
   });
 
   await page.setContent(htmlString, { waitUntil: "networkidle0" });
@@ -45,12 +52,11 @@ async function _screenshotHtmlToFile(page: puppeteer.Page, htmlString: string, o
     path: outputPath,
     type: 'webp',
     omitBackground: false,
-    quality: 100,
+    quality: 90,
   });
-  ``;
 }
 
-export async function createImages(db:any) {
+export async function createImages(db: any) {
   const dir = './images';
   if (!fs.existsSync(dir)) { fs.mkdirSync(dir, { recursive: true }); }
 
@@ -59,34 +65,39 @@ export async function createImages(db:any) {
 
   try {
     while (true) {
+
       const record = await _claimNextClusterPost(db);
       if (!record) break;
 
-      const { cluster_id, html_twitter, html_bluesky } = record;
+      const { cluster_id, html_twitter, html_bluesky, was_posted_twitter, was_posted_bluesky } = record;
       console.info(`Creating images for cluster_id:${cluster_id}`);
 
-      const twitterPath = path.join(dir, `${cluster_id}_twitter.png`);
-      const blueskyPath = path.join(dir, `${cluster_id}_bluesky.png`);
+      const twitterPath = path.join(dir, `${cluster_id}_twitter.webp`);
+      const blueskyPath = path.join(dir, `${cluster_id}_bluesky.webp`);
 
       // Twitter
-      await _screenshotHtmlToFile(page, html_twitter, twitterPath, 'twitter');
-      await db.setData(
-        `UPDATE cluster_post
-         SET was_posted_twitter = 'image_created' 
-         WHERE cluster_id = ?`,
-        [cluster_id]
-      );
+      if (was_posted_twitter === 'processing_image') { // processing_image === no image yet
+        await _screenshotHtmlToFile(page, html_twitter, twitterPath, 'twitter');
+        await db.setData(
+          `UPDATE cluster_post
+           SET was_posted_twitter = 'image_created' 
+           WHERE cluster_id = ?`,
+          [cluster_id]
+        );
+        console.info(`Twitter image created for cluster_id:${cluster_id}`);
+      }
 
       // Bluesky
-      await _screenshotHtmlToFile(page, html_bluesky, blueskyPath, 'bluesky');
-      await db.setData(
-        `UPDATE cluster_post
-         SET was_posted_bluesky = 'image_created' 
-         WHERE cluster_id = ?`,
-        [cluster_id]
-      );
-
-      console.info(`Images created for cluster_id:${cluster_id}`);
+      if (was_posted_bluesky === 'processing_image') {
+        await _screenshotHtmlToFile(page, html_bluesky, blueskyPath, 'bluesky');
+        await db.setData(
+          `UPDATE cluster_post
+           SET was_posted_bluesky = 'image_created' 
+           WHERE cluster_id = ?`,
+          [cluster_id]
+        );
+        console.info(`Bluesky image created for cluster_id:${cluster_id}`);
+      }
     }
   } catch (err) {
     console.error(err);
